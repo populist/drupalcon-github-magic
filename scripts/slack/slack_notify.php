43a46d1bb3bd0b506c7859ff8970ab464962369c
Matt Cheney <matt@getpantheon.com>
2017-04-11 19:27:39 -0700

    Updating Slack Icons to Current Site

--- scripts/slack/slack_notify.php
+++ scripts/slack/slack_notify.php
@@ -9,7 +9,7 @@ $slack_type = $argv[1]; // Argument One
 switch($slack_type) {
   case 'behat': 
     $slack_agent = 'Behat';
-    $slack_icon = 'http://live-drupalcon-nola-demo.pantheonsite.io/sites/default/files/icons/behat.png';
+    $slack_icon = 'http://live-drupalcon-github-magic.pantheonsite.io/sites/default/files/icons/behat.png';
     $slack_color = '#00ff00';
     $slack_message = 'Kicking off Behavioral Testing with Behat...';
     _slack_tell( $slack_message, 'drupalcon', $slack_agent, $slack_icon, $slack_color);
@@ -19,14 +19,14 @@ switch($slack_type) {
     break;
   case 'behat_finished':
     $slack_agent = 'Behat';
-    $slack_icon = 'http://live-drupalcon-nola-demo.pantheonsite.io/sites/default/files/icons/behat.png';
+    $slack_icon = 'http://live-drupalcon-github-magic.pantheonsite.io/sites/default/files/icons/behat.png';
     $slack_color = '#00ff00';
     $slack_message = 'Testing complete. Build: *PASSED*';
     _slack_tell( $slack_message, 'drupalcon', $slack_agent, $slack_icon, $slack_color);
     break;
   case 'composer':
     $slack_agent = 'Composer';
-    $slack_icon = 'http://live-drupalcon-nola-demo.pantheonsite.io/sites/default/files/icons/composer.png';
+    $slack_icon = 'http://live-drupalcon-github-magic.pantheonsite.io/sites/default/files/icons/composer.png';
     $slack_color = '#000080';
     $slack_message = 'Running Composer to create build artifact...';
     _slack_tell( $slack_message, 'drupalcon', $slack_agent, $slack_icon, $slack_color);
@@ -37,7 +37,7 @@ switch($slack_type) {
     break;
   case 'circle':
     $slack_agent = 'CircleCI';
-    $slack_icon = 'http://live-drupalcon-nola-demo.pantheonsite.io/sites/default/files/icons/circle.png';
+    $slack_icon = 'http://live-drupalcon-github-magic.pantheonsite.io/sites/default/files/icons/circle.png';
     $slack_color = '#229922';
     $slack_message = 'New code is detected! Time to kick off a new build...';
     _slack_tell( $slack_message, 'drupalcon', $slack_agent, $slack_icon, $slack_color);
@@ -50,7 +50,7 @@ switch($slack_type) {
     break;
   case 'sass':
     $slack_agent = 'sass';
-    $slack_icon = 'http://live-drupalcon-nola-demo.pantheonsite.io/sites/default/files/icons/sass.png';
+    $slack_icon = 'http://live-drupalcon-github-magic.pantheonsite.io/sites/default/files/icons/sass.png';
     $slack_color = '#cc6699';
     $slack_message = 'Running CSS compilation using SASS...';
     _slack_tell( $slack_message, 'drupalcon', $slack_agent, $slack_icon, $slack_color);
@@ -62,7 +62,7 @@ switch($slack_type) {
     break;
   case 'terminus':
     $slack_agent = 'Terminus';
-    $slack_icon = 'http://live-drupalcon-nola-demo.pantheonsite.io/sites/default/files/icons/terminus.png';
+    $slack_icon = 'http://live-drupalcon-github-magic.pantheonsite.io/sites/default/files/icons/terminus.png';
     $slack_color = '#1ec503';
     $slack_message = "Authenticating to Pantheon with Terminus machine token...";
     _slack_tell( $slack_message, 'drupalcon', $slack_agent, $slack_icon, $slack_color);
@@ -73,7 +73,7 @@ switch($slack_type) {
     break;
   case 'pantheon_multidev':
     $slack_agent = 'Terminus';
-    $slack_icon = 'http://live-drupalcon-nola-demo.pantheonsite.io/sites/default/files/icons/terminus.png';
+    $slack_icon = 'http://live-drupalcon-github-magic.pantheonsite.io/sites/default/files/icons/terminus.png';
     $slack_color = '#1ec503';
     $slack_message = "Setting up Pantheon Multidev testing environment...";
     _slack_tell( $slack_message, 'drupalcon', $slack_agent, $slack_icon, $slack_color);
@@ -86,7 +86,7 @@ switch($slack_type) {
     break;
   case 'pantheon_dev':
     $slack_agent = 'Terminus';
-    $slack_icon = 'http://live-drupalcon-nola-demo.pantheonsite.io/sites/default/files/icons/terminus.png';
+    $slack_icon = 'http://live-drupalcon-github-magic.pantheonsite.io/sites/default/files/icons/terminus.png';
     $slack_color = '#1ec503';
     $slack_message = "Setting up Pantheon Dev environment...";
     _slack_tell( $slack_message, 'drupalcon', $slack_agent, $slack_icon, $slack_color);
