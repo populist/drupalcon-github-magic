d5b7c7f019594ae0076b913f9f1126749696049a
Matt Cheney <matt@getpantheon.com>
2017-04-12 11:21:28 -0700

    Adding Behat Testing into Demo

--- scripts/slack/slack_notify.php
+++ scripts/slack/slack_notify.php
@@ -10,18 +10,18 @@ switch($slack_type) {
   case 'behat': 
     $slack_agent = 'Behat';
     $slack_icon = 'http://live-drupalcon-github-magic.pantheonsite.io/sites/default/files/icons/behat.png';
-    $slack_color = '#00ff00';
+    $slack_color = '#0000000';
     $slack_message = 'Kicking off Behavioral Testing with Behat...';
     _slack_tell( $slack_message, 'drupalcon', $slack_agent, $slack_icon, $slack_color);
     $slack_message = array();
-    $slack_message['Tests'] = 'TODO, TODO2';
+    $slack_message['Test'] = '*Scenario*: A user should see "El Museo de Arte" on the homepage' . "\n" . '     *Given* I am on the homepage' . "\n" .  '      *Then* I should see the text "El Museo de Arte"';
     _slack_tell( $slack_message, 'drupalcon', $slack_agent, $slack_icon, $slack_color);
     break;
   case 'behat_finished':
     $slack_agent = 'Behat';
     $slack_icon = 'http://live-drupalcon-github-magic.pantheonsite.io/sites/default/files/icons/behat.png';
     $slack_color = '#00ff00';
-    $slack_message = 'Testing complete. Build: *PASSED*';
+    $slack_message = 'Testing result: *Build PASSED*';
     _slack_tell( $slack_message, 'drupalcon', $slack_agent, $slack_icon, $slack_color);
     break;
   case 'composer':
